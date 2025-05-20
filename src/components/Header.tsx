f6d8abfa3d06401ed1a2c0098126f112594ef7b4
Haruto Ikehara <ikehara@vectis.co.jp>
2025-05-20 17:22:03 +0900

    ヘッダーのz-indexの修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <header className="fixed top-0 left-0 flex justify-center items-center w-full h-14 bg-opacity-70 backdrop-blur backdrop-saturate-[180%] shadow-md">
+      <header className="fixed top-0 left-0 z-20 flex justify-center items-center w-full h-14 bg-opacity-70 backdrop-blur backdrop-saturate-[180%] shadow-md">
         <h1 className="text-lg font-semibold">MVV暗記アプリ</h1>
       </header>
       <div className="h-14" />
