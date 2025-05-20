b09d4f24a5535ad0dbd1bb4ea7d9732d73143a61
Haruto Ikehara <ikehara@vectis.co.jp>
2025-05-20 16:53:41 +0900

    ポート番号の修正 (#4)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 export default function Header() {
   return (
-    <header className="flex justify-center items-center h-14 bg-base-200 shadow-md">
-      <h1 className="text-lg font-semibold">MVV暗記アプリ</h1>
-    </header>
+    <>
+      <header className="fixed top-0 left-0 flex justify-center items-center w-full h-14 bg-opacity-70 backdrop-blur backdrop-saturate-[180%] shadow-md">
+        <h1 className="text-lg font-semibold">MVV暗記アプリ</h1>
+      </header>
+      <div className="h-14" />
+    </>
   );
 }
