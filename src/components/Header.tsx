1c51b64dada870b24e4922c9df1c0cb6a5dd83f9
Haruto Ikehara <ikehara@vectis.co.jp>
2025-05-20 16:52:24 +0900

    ヘッダーのスタイルを修正

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <header className="fixed top-0 left-0 flex justify-center items-center w-full h-14 bg-opacity-70 backdrop-blur backdrop-saturate-[180%] shadow-md">
+        <h1 className="text-lg font-semibold">MVV暗記アプリ</h1>
+      </header>
+      <div className="h-14" />
+    </>
+  );
+}
